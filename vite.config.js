ef89655d4802b3c9ab83b55cb15259e1ecd50052
Tianyu (Sky) Lu <tianyu@lu.fm>
2025-06-08 11:01:28 -0500

    1

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: process.env.NODE_ENV === 'production' ? '/DoxBench-LeaderBoard/' : '/',
+  base: process.env.NODE_ENV === 'production' ? '/doxing-via-lens/' : '/',
   server: {
     port: 3000,
     open: true
